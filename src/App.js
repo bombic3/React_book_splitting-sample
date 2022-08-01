a3d95d298996dac6307bb37f1755591b6b50e339
bombi <bombic3@naver.com>
2022-08-01 17:51:57 +0900

    TEST : import()함수 사용하여 빌드작업

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+// import notify from './notify';
 
-function App() {
+function App () {
+  const onClick = () => {
+    import('./notify').then(result => result.default());
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          수정 : Hello React!
-        </p>
+        <p onClick={onClick}>Hello React!</p>
       </header>
     </div>
   );
